467e7bad06ce0185196fef1ccb3c7d936313e1a5
Margarita <m.spiricheva@yandex.ru>
2024-05-22 18:17:43 +0300

    Add choice of greeting and exit

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,15 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.getName();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n0 - Exit");
+        int choice = in.nextInt();
+        System.out.println("Your choice: " + choice);
+        if (choice == 1) {
+            Cli.getName();
+        }
     }
 }
