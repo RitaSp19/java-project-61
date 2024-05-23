ef341469fc8299021063b9cc975b63f3e49ed847
Margarita <m.spiricheva@yandex.ru>
2024-05-23 19:26:06 +0300

    Fix line break

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Cli {
     public static void getName() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!\nMay I have your name?");
+        System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
         String name = in.nextLine();
-
         System.out.println("Hello, " + name + "!");
+        in.close();
     }
 }
