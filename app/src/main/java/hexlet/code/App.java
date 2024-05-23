f76a8abb2f17c41b8fd3135d6f3b6b6aba643441
Margarita <m.spiricheva@yandex.ru>
2024-05-23 19:24:39 +0300

    Add Even to games list

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
         int choice = in.nextInt();
         System.out.println("Your choice: " + choice);
         if (choice == 1) {
             Cli.getName();
+        } else if (choice == 2) {
+            Even.evenGame();
         }
     }
 }
