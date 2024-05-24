09e809412012a9ca9cc7307030b701d94be826bb
Margarita <m.spiricheva@yandex.ru>
2024-05-24 18:17:43 +0300

    Fix a bug in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,8 +6,8 @@ public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
+        System.out.print("Your choice: ");
         int choice = in.nextInt();
-        System.out.println("Your choice: " + choice);
         if (choice == 1) {
             Cli.getName();
         } else if (choice == 2) {
