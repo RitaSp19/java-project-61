eb3098f7e936946aefc9056d3785cf18e65054ce
Margarita <m.spiricheva@yandex.ru>
2024-06-04 22:28:56 +0300

    Add Calc game selection

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,27 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
         System.out.print("Your choice: ");
         int choice = in.nextInt();
-        if (choice == 1) {
-            Cli.getName();
-        } else if (choice == 2) {
-            Even.evenGame();
+        switch (choice) {
+            case 1:
+                Cli.getName();
+                break;
+            case 2:
+                Even.playEven();
+                break;
+            case 3:
+                Calc.playCalc();
+            default:
+                break;
         }
     }
 }
