43c414e2be57a4ac34214627de0f654bdfa5d62d
Margarita <m.spiricheva@yandex.ru>
2024-06-05 12:40:58 +0300

    Fix bug in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter." +
                 "\n1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n0 - Exit");
         System.out.print("Your choice: ");
