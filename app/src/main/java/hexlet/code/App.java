dcaba8828abec68d2e14bcd22941a3f58b9cbfcc
Margarita <m.spiricheva@yandex.ru>
2024-06-05 12:31:31 +0300

    Update App.java - add GCD game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,13 +2,14 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
 
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter." +
+                "\n1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n0 - Exit");
         System.out.print("Your choice: ");
         int choice = in.nextInt();
         switch (choice) {
@@ -20,6 +21,10 @@ public class App {
                 break;
             case 3:
                 Calc.playCalc();
+                break;
+            case 4:
+                GCD.playGCD();
+                break;
             default:
                 break;
         }
